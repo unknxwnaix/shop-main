2abfd96f12f10c648386dd5f8c6566b5e1649cc7
Xt4lll <isip_s.a.ushakov@mpt.ru>
2024-07-24 20:03:13 +0300

    вф

---
+++ client/ProductManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace client
+{
+    public static class ProductManager
+    {
+        public static ObservableCollection<Product> products { get; set; }
+        public static ObservableCollection<Product> displayedProducts { get; set; }
+
+        static ProductManager()
+        {
+            products = new ObservableCollection<Product>();
+            displayedProducts = new ObservableCollection<Product>();
+        }
+    }
+}
